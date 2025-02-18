8b2bf417814e572288021f2cbf9d58bf54d28502
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2025-02-19 00:47:00 +0100

    railways domain added to the backend

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect(process.env.MONGO_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
+
+app.set('trust proxy', true);
 
 const allowedOrigins = [
   "http://localhost:5173", // For local development
