7e6f986e812db1eade4d740b4663139b842d3d73
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2025-02-19 13:12:40 +0100

    profile picture works for side bar main

--- frontend/src/templates/dashboard/components/SideMenu.tsx
+++ frontend/src/templates/dashboard/components/SideMenu.tsx
@@ -59,7 +59,7 @@ export default function SideMenu() {
           <Avatar
             sizes="small"
             alt={user?.name || 'User'}
-            src={`http://localhost:3000${user?.profilePicture}` || ''}
+            src={`http://localhost:3000${user?.profilePicture}` || `https://easy-email-production.up.railway.app${user?.profilePicture}`}
             sx={{ width: 36, height: 36 }}
           />
           <Box sx={{ mr: 'auto' }}>
