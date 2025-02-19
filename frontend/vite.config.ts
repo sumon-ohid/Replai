c15c455a2504e6d109fb6f49b0ff22207eb117e4
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2025-02-19 19:16:37 +0100

    unneccesary changes for tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+    ],
     base: mode === 'production' ? 'https://easy-email-production.up.railway.app/' : '/',
     server: {
       allowedHosts: ['email-agent.up.railway.app', 'divine-forgiveness-production.up.railway.app'],
