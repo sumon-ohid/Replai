fe5509c792131082f28b8d6974f90f94627d049f
sumon-ohid <smd-ohid@student.42wolfsburg.de>
2025-02-19 22:34:50 +0100

    home page lets get started button change

--- frontend/src/templates/marketing-page/components/Hero.tsx
+++ frontend/src/templates/marketing-page/components/Hero.tsx
@@ -1,63 +1,72 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import InputLabel from '@mui/material/InputLabel';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import visuallyHidden from '@mui/utils/visuallyHidden';
-import { styled } from '@mui/material/styles';
-import CustomizedTimeline from './Timeline';
-import Chip from '@mui/material/Chip';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import InputLabel from "@mui/material/InputLabel";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import visuallyHidden from "@mui/utils/visuallyHidden";
+import { styled } from "@mui/material/styles";
+import CustomizedTimeline from "./Timeline";
+import Chip from "@mui/material/Chip";
+import { useNavigate } from "react-router-dom";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
-const StyledBox = styled('div')(({ theme }) => ({
-  alignSelf: 'center',
-  width: '100%',
+const StyledBox = styled("div")(({ theme }) => ({
+  alignSelf: "center",
+  width: "100%",
   height: 400,
   marginTop: theme.spacing(8),
   borderRadius: theme.shape.borderRadius,
-  outline: '6px solid',
-  outlineColor: 'hsla(220, 25%, 80%, 0.2)',
-  border: '1px solid',
+  outline: "6px solid",
+  outlineColor: "hsla(220, 25%, 80%, 0.2)",
+  border: "1px solid",
   borderColor: theme.palette.grey[200],
-  boxShadow: '0 0 12px 8px hsla(220, 25%, 80%, 0.2)',
-  backgroundImage: `url(${import.meta.env.VITE_TEMPLATE_IMAGE_URL || 'https://mui.com'}/static/screenshots/material-ui/getting-started/templates/dashboard.jpg)`,
-  backgroundSize: 'cover',
-  [theme.breakpoints.up('sm')]: {
+  boxShadow: "0 0 12px 8px hsla(220, 25%, 80%, 0.2)",
+  backgroundImage: `url(${
+    import.meta.env.VITE_TEMPLATE_IMAGE_URL || "https://mui.com"
+  }/static/screenshots/material-ui/getting-started/templates/dashboard.jpg)`,
+  backgroundSize: "cover",
+  [theme.breakpoints.up("sm")]: {
     marginTop: theme.spacing(10),
     height: 700,
   },
-  ...theme.applyStyles('dark', {
-    boxShadow: '0 0 24px 12px hsla(210, 100%, 25%, 0.2)',
-    backgroundImage: `url(${import.meta.env.VITE_TEMPLATE_IMAGE_URL || 'https://mui.com'}/static/screenshots/material-ui/getting-started/templates/dashboard-dark.jpg)`,
-    outlineColor: 'hsla(220, 20%, 42%, 0.1)',
+  ...theme.applyStyles("dark", {
+    boxShadow: "0 0 24px 12px hsla(210, 100%, 25%, 0.2)",
+    backgroundImage: `url(${
+      import.meta.env.VITE_TEMPLATE_IMAGE_URL || "https://mui.com"
+    }/static/screenshots/material-ui/getting-started/templates/dashboard-dark.jpg)`,
+    outlineColor: "hsla(220, 20%, 42%, 0.1)",
     borderColor: theme.palette.grey[700],
   }),
 }));
 
 export default function Hero() {
+  const navigate = useNavigate();
+  const isDarkMode = localStorage.getItem("mui-mode") === "dark";
+
   return (
     <Box
       id="hero"
       sx={(theme) => ({
-        width: '100%',
-        backgroundRepeat: 'no-repeat',
+        width: "100%",
+        backgroundRepeat: "no-repeat",
 
         backgroundImage:
-          'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 90%), transparent)',
-        ...theme.applyStyles('dark', {
+          "radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 90%), transparent)",
+        ...theme.applyStyles("dark", {
           backgroundImage:
-            'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 16%), transparent)',
+            "radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 16%), transparent)",
         }),
       })}
     >
       <Container
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
           pt: { xs: 14, sm: 20 },
           pb: { xs: 8, sm: 12 },
         }}
@@ -65,15 +74,15 @@ export default function Hero() {
         <Stack
           spacing={2}
           useFlexGap
-          sx={{ alignItems: 'center', width: { xs: '100%', sm: '70%' } }}
+          sx={{ alignItems: "center", width: { xs: "100%", sm: "70%" } }}
         >
           <Typography
             variant="h1"
             sx={{
-              display: 'flex',
-              flexDirection: { xs: 'column', sm: 'row' },
-              alignItems: 'center',
-              fontSize: 'clamp(3rem, 10vw, 3.5rem)',
+              display: "flex",
+              flexDirection: { xs: "column", sm: "row" },
+              alignItems: "center",
+              fontSize: "clamp(3rem, 10vw, 3.5rem)",
             }}
           >
             Our&nbsp;latest&nbsp;
@@ -81,65 +90,72 @@ export default function Hero() {
               component="span"
               variant="h1"
               sx={(theme) => ({
-                fontSize: 'inherit',
-                color: 'primary.main',
-                ...theme.applyStyles('dark', {
-                  color: 'primary.light',
+                fontSize: "inherit",
+                color: "primary.main",
+                ...theme.applyStyles("dark", {
+                  color: "primary.light",
                 }),
               })}
             >
-            email agent
+              email agent
             </Typography>
           </Typography>
           <Typography
             sx={{
-              textAlign: 'center',
-              color: 'text.secondary',
-              width: { sm: '100%', md: '80%' },
+              textAlign: "center",
+              color: "text.secondary",
+              width: { sm: "100%", md: "80%" },
             }}
           >
-          No more frastration with your emails. If you are tired of managing your emails, we are here to help.
-          We will handle your emails so you can do things you love. Each email will be analized and replied using AI. 
-          Fast and easy.
+            No more frastration with your emails. If you are tired of managing
+            your emails, we are here to help. We will handle your emails so you
+            can do things you love. Each email will be analized and replied
+            using AI. Fast and easy.
           </Typography>
           <CustomizedTimeline />
           <Stack
-            direction={{ xs: 'column', sm: 'row' }}
+            direction={{ xs: "column", sm: "row" }}
             spacing={1}
             useFlexGap
-            sx={{ pt: 2, width: { xs: '100%', sm: '350px' } }}
+            sx={{
+              pt: 2,
+              width: {
+                xs: "100%",
+                sm: "350px",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+              mt: 5,
+            }}
           >
-            <InputLabel htmlFor="email-hero" sx={visuallyHidden}>
-              Email
-            </InputLabel>
-            <TextField
-              id="email-hero"
-              hiddenLabel
-              size="small"
-              variant="outlined"
-              aria-label="Enter your email address"
-              placeholder="Your email address"
-              fullWidth
-              slotProps={{
-                htmlInput: {
-                  autoComplete: 'off',
-                  'aria-label': 'Enter your email address',
-                },
-              }}
-            />
-            <Button
+            {/* <Button
               variant="contained"
               color="primary"
               size="small"
               sx={{ minWidth: 'fit-content' }}
+              onClick={() => {
+                navigate('/signin');
+              }}
             >
               Start now
-            </Button>
+            </Button> */}
+
+          <button className="relative inline-flex h-12 active:scale-95 transition overflow-hidden rounded-lg p-[1px] focus:outline-none"
+            onClick={() => {
+                navigate('/signin');
+              }}>
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,rgb(75,165,255)_0%,rgb(120,190,255)_50%,rgb(50,140,255)_100%)]"></span>
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2">
+              <span>Let's get started</span>
+              <ArrowForwardIcon />
+            </span>
+          </button>
+
           </Stack>
           <Typography
             variant="caption"
             color="text.secondary"
-            sx={{ textAlign: 'center' }}
+            sx={{ textAlign: "center" }}
           >
             By clicking &quot;Start now&quot; you agree to our&nbsp;
             <Link href="#" color="primary">
@@ -153,4 +169,3 @@ export default function Hero() {
     </Box>
   );
 }
-
